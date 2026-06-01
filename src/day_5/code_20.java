5afb112051bf1f8ae1b735214ade7f144748353b
ANUJ PANDEY <anujpandey20007@gmail.com>
2026-06-01 12:25:04 +0530

    ADDED DAY 5 CODES

---
+++ src/day_5/code_20.java
@@ -0,0 +1,35 @@
+package day_5;
+
+import java.util.Scanner;
+
+public class code_20 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        long number = input.nextLong();
+
+        long originalNumber = number;
+        long largestPrimeFactor = -1;
+
+        while (number % 2 == 0) {
+            largestPrimeFactor = 2;
+            number /= 2;
+        }
+        for (long i = 3; i * i <= number; i += 2) {
+            while (number % i == 0) {
+                largestPrimeFactor = i;
+                number /= i;
+            }
+        }
+        if (number > 2) {
+            largestPrimeFactor = number;
+        }
+        if (originalNumber < 2) {
+            System.out.println(originalNumber + " does not have any prime factors.");
+        } else {
+            System.out.println("The largest prime factor of " + originalNumber + " is: " + largestPrimeFactor);
+        }
+
+        input.close();
+    }
+}
