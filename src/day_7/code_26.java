5e87871c7f482118c284bd3a073992b8af095b3c
ANUJ PANDEY <anujpandey20007@gmail.com>
2026-06-03 10:59:00 +0530

    ADDED DAY 7  CODES

---
+++ src/day_7/code_26.java
@@ -0,0 +1,24 @@
+package day_7;
+
+import java.util.Scanner;
+
+public class code_26 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter number ");
+        int num = input.nextInt();
+        int result = fibo(num);
+        System.out.println("result "+ result);
+    }
+    public static int fibo(int num){
+        int a = 0 ;
+        int b = 1 ;
+        if (num == 0 ){
+            return 0;
+        } else if (num == 1 ) {
+            return 1 ;
+        }
+        int result = fibo(num-1)+ fibo(num-2);
+        return result;
+    }
+}
