cade0ac54a00fe28b2dd1e4d63d17c5335fbaade
ANUJ PANDEY <anujpandey20007@gmail.com>
2026-06-05 20:49:58 +0530

    ADDED DAY 7  CODES

--- src/day_8/code_31.java
+++ src/day_8/code_31.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 
 public class code_31 {
     public static void main(String[] args) {
-        //half pyraid pattern
+        //half pyramid pattern
         Scanner input = new Scanner(System.in);
         System.out.print("enter number-->");
         int num = input.nextInt();
         for (int i = 0; i <= num; i++) {
-            for (int j = 0; j < i; j++) {
+            for (int j = 0; j <= i; j++) {
                 System.out.print((char) ('A'+j));
 
             }
